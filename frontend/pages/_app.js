61c09e668e7e3d2cb7f4b2c2937ee4d7aa5ee0d9
kamal0620040 <neupane0620040@outlook.com>
2022-10-06 13:04:49 +0545

    Reusable Logic

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import { Navbar, Footer } from '../components';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider className="class">
+    <div className="dark:bg-mart-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/a7110d492e.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
 
-export default MyApp
+export default MyApp;
