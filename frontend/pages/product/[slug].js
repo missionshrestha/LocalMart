17b551644acbed3dc39c1f810d7aa92eec75c4b8
kamal0620040 <neupane0620040@outlook.com>
2022-10-10 11:31:59 +0545

    Fetched ProductDetails in Specific Product Page Except product features

--- frontend/pages/product/[slug].js
+++ frontend/pages/product/[slug].js
@@ -1,14 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
+import { useRouter } from 'next/router';
+import axios from 'axios';
 import Button from '../../components/Button';
 import images from '../../assets';
+import { calculateDiscount } from '../../utils/calculateDiscount';
+
+const headers = {
+  withCredentials: true,
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Credentials': true,
+};
+
+const baseURL = process.env.NEXT_PUBLIC_BACKEND_API;
 
 const ProductDetail = () => {
+  const [product, setProduct] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
+  const [mainImage, setMainImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const { theme } = useTheme();
   const [selected, setSelected] = useState(0);
-  //   const { query } = useRouter();
-  // fetch graphql data
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    const { slug } = router.query;
+    axios.get(`${baseURL}/product/${slug}`, { headers }).then((response) => {
+      console.log(response.data);
+      setProduct(response.data);
+      setImageUrls(response.data.image_url);
+      setMainImage(response.data.image_url[0].url.startsWith('https://') ? response.data.image_url[0].url : `https://${response.data.image_url[0].url}`);
+      setIsLoading(false);
+    }).catch((error) => {
+      console.log(error);
+      setIsLoading(false);
+    });
+  }, [router.isReady]);
+
+  const updater = (idx) => {
+    const newImageUrls = imageUrls.map((im, index) => {
+      if (index === idx) {
+        return { url: 'https://i.ibb.co/71RgwLQ/default-Product.png' };
+      }
+      return im;
+    });
+    setImageUrls(newImageUrls);
+    setMainImage(newImageUrls[0].url);
+  };
+
+  if (!product) return 'No post!';
 
   return (
     <div className="flex flex-col justify-end sm:px-4 p-12 pt-16">
@@ -56,26 +98,26 @@ const ProductDetail = () => {
       <div className="hidden mt-6 w-full md:flex flex-col gap-10">
         <div className="flex justify-around h-64">
           <div className="basis-1/4 flex flex-col flex-wrap gap-2 overflow overflow-x-scroll no-scrollbar">
-            {[1, 2, 3, 4].map((item, idx) => (
-              <div onClick={() => { setSelected(idx); }} className={`${selected === idx && 'border-2 border-logo-green rounded-2xl'} bg-white relative w-full h-1/3 hover:border-2 cursor-pointer`}>
-                <Image className="rounded-2xl" src="https://i.ibb.co/82qS39N/RE4LiWr.jpg" layout="fill" objectFit="contain" />
+            {product.image_url.map((item, idx) => (
+              <div onClick={() => { setSelected(idx); setMainImage(imageUrls[idx].url); }} className={`${selected === idx && 'border-2 border-logo-green rounded-2xl'} bg-white relative w-full h-1/3 hover:border-2 cursor-pointer`}>
+                <Image className="rounded-2xl" src={imageUrls[idx].url.startsWith('https://') ? imageUrls[idx].url : `https://${imageUrls[idx].url}`} onError={() => { updater(idx); }} layout="fill" objectFit="contain" />
               </div>
             ))}
           </div>
           <div className="border-2 border-logo-green rounded-2xl basis-1/2 relative w-full h-full">
-            <div className="z-10 absolute bg-red-600 px-2 py-1 rounded-2xl top-4 -right-8 text-white font-montserrat text-sm font-semibold">50% OFF</div>
-            <Image className="rounded-2xl" src="https://i.ibb.co/82qS39N/RE4LiWr.jpg" layout="fill" objectFit="contain" />
+            {product.discount_percentage > 0 && <div className="z-10 absolute bg-red-600 px-2 py-1 rounded-2xl top-4 -right-8 text-white font-montserrat text-sm font-semibold">{product.discount_percentage}% OFF</div>}
+            <Image className="rounded-2xl" src={mainImage} layout="fill" objectFit="contain" />
           </div>
         </div>
         <div className="basis-1/2 flex flex-col justify-center items-center">
           <div className="flex flex-col">
-            <p className="text-subtitle-blue italic font-semibold text-base md:mb-4 sm:mb-1 xs:ml-0">- The Electronics</p>
-            <h1 className="text-4xl font-bold">Dell XP13 Laptop</h1>
+            <p className="text-subtitle-blue italic font-semibold text-base md:mb-4 sm:mb-1 xs:ml-0">- The {product.tags}</p>
+            <h1 className="text-4xl font-bold">{product.title}</h1>
             <div className="mt-5 flex">
-              <span className="bg-gray-600 px-4 py-2 rounded-2xl text-white font-montserrat text-sm font-semibold">GADGET</span>
+              <span className="bg-gray-600 px-4 py-2 rounded-2xl text-white font-montserrat text-sm font-semibold">{product.tags.toUpperCase()}</span>
               <div className="flex gap-3 ml-8 text-2xl">
-                <div className="text text-mart-gray-2 line-through">$1500</div>
-                <div className="font-semibold">$725</div>
+                {product.discount_percentage > 0 && <div className="text text-mart-gray-2 line-through">${product.price}</div>}
+                <div className="font-semibold">${calculateDiscount(product.price, product.discount_percentage)}</div>
               </div>
             </div>
             <div className="flex gap-10 items-center mt-8">
--- frontend/utils/calculateDiscount.js
+++ frontend/utils/calculateDiscount.js
@@ -1,1 +1,1 @@
-export const calculateDiscount = (price, discount) => (price - (discount / 100) * price).toFixed();
+export const calculateDiscount = (price, discount) => (price - ((discount / 100) * price)).toFixed();
