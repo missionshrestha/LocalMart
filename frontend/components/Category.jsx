282d2bb88c8482eebe53506db398b9b19ab44a9f
kamal0620040 <neupane0620040@outlook.com>
2022-11-21 10:04:43 +0545

    Added Category Detail Page

--- frontend/components/Category.jsx
+++ frontend/components/Category.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const Category = ({ title, logo }) => (
   <div className="flex flex-col justify-center bg-banner-light w-32 p-7 cursor-pointer mr-8 my-4 rounded-2xl dark:bg-banner-dark">
-    <Link href="/">
+    <Link href={`/category/${title.toLowerCase()}`}>
       <div className="flex flex-col justify-center items-center">
         <Image src={logo} height={32} width={32} href="#" alt="logo" />
         <h1 className="text-xl mt-4 font-semibold">{title}</h1>
