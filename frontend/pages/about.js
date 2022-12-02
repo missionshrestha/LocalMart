8f5d3e525844db21a676e5a76e2eb74517a7c2a5
Mission Shretha <missionshrestha99@gmail.com>
2022-12-02 10:15:45 +0545

    added contact & about page

---
+++ frontend/pages/about.js
@@ -0,0 +1,43 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+const about = () => (
+  <div className="  pt-20 flex flex-col w-full justify-center ">
+    <h1 className="font-montserrat dark:text-green-400 text-mart-black-1 text-3xl minlg:text-4xl xs:text-xl font-bold ml-4 xs:ml-0">Who are We, Locals Mart:</h1>
+    <p className="p-4">
+      The market for e-commerce sites has enormous potential as a result of Nepal explosion in the number of internet users and digital payment methods. There were 822 thousand more internet users in Nepal between 2021 and 2022.
+      Peoples purchasing and use of goods and services has changed since the advent of e-commerce. People are increasingly using computers and other electronic devices to place orders for items that will be delivered to their homes. As a result, the retail environment has shifted. The massive success of Daraz and SastoDeal has caused traditional retailers to change their business practices. eCommerce transactions are expected to increase from 14.1 per cent to 22 per cent by 2023. According to these projections, the e-commerce industry has yet to reach its full potential.
+
+    </p>
+    <p className="p-4">
+      Shopping locally is the best way to conserve the environment while also helping local communities. People support their local economy, essential jobs are created and maintained, families are supported, and the community and culture are reinforced by purchasing food and other products made close to home.
+      Locals Mart simplifies the ordering and delivery process for customers who want to support local products by providing superior UI and UX, an easy ordering procedure, robust delivery, and a safe and reliable system. The website features interactive and up-to-date items that include all available alternatives.
+    </p>
+
+    <h1 className="font-montserrat dark:text-green-400 text-mart-black-1 text-3xl minlg:text-4xl xs:text-xl font-bold ml-4 xs:ml-0">Objectives</h1>
+    <p className="p-4">
+      With functionality and practicality being the key factors tied to time constraints, we intend to keep our project on a limited scale for the time being, focusing only on providing a platform for locals to promote their items to thousands of online consumers. Our main goal is to broaden our knowledge and skills in web development and database management, as well as to practically design an appealing UI and UX so that our users can easily see and find their preferred products. However, to attain this goal as successfully as possible, we must first set certain pre-planned objectives before beginning the actual activity.
+    </p>
+
+    <p className="pt-2 pb-2 pl-4 pr-4">
+      1) To provide a platform for the local people to sell their products online
+
+    </p>
+    <p className="pt-2 pb-2 pl-4 pr-4">
+      2) To make it accessible for Nepalese as well as foreigners to find authentic Nepali local products.
+
+    </p>
+    <p className="pt-2 pb-2 pl-4 pr-4">
+
+      3) To aid local merchants so they can compete with abrasive marketing tactics from large internet merchants and national or international businesses
+
+    </p>
+    <p className="pt-2 pb-2 pl-4 pr-4">
+      4) To provide a single platform for various product genres and so on.
+
+    </p>
+
+  </div>
+);
+
+export default about;
---
+++ frontend/pages/contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const contact = () => (
+  <div className="  pt-20 flex flex-col w-full justify-center ">
+    <h1 className="pt-16 font-montserrat dark:text-green-400 text-mart-black-1 text-3xl minlg:text-4xl xs:text-xl font-bold ml-4 xs:ml-0 text-center">OUR NUMBER:</h1>
+    <p className=" pt-4 pb-4 font-montserrat dark:text-white text-mart-black-1 text-3xl minlg:text-4xl xs:text-xl font-thin ml-4 xs:ml-0 text-center">
+      987654321
+    </p>
+
+    <h1 className="pt-16 font-montserrat dark:text-green-400 text-mart-black-1 text-3xl minlg:text-4xl xs:text-xl font-bold ml-4 xs:ml-0 text-center">OUR EMAIL:</h1>
+    <p className="pt-4 pb-4 font-montserrat dark:text-white text-mart-black-1 text-3xl minlg:text-4xl xs:text-xl font-thin ml-4 xs:ml-0 text-center">
+      localmart@yopmail.com
+    </p>
+
+  </div>
+);
+
+export default contact;
